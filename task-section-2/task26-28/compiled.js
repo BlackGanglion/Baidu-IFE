c2caed2fa2b0369b9fe69f2906c0202db7c81690
BlackGanglion <BlackGanglion@blackganglion.com>
2016-04-09 00:37:29 +0800

    update task26-28
    update task26-28

--- task-section-2/task26-28/compiled.js
+++ task-section-2/task26-28/compiled.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -9,42 +9,155 @@ var orbitRadius = 125,
     mainWidth = 380,
     shipCount = 4;
 
-var shipDOM = document.getElementsByClassName('ship'),
-    controlList = document.getElementById('control-list');
+var TIME_DEFER = 300,
+    PACKET_LOSS = 10;
+
+var shipDOM = document.getElementsByClassName("ship"),
+    shipControlList = document.getElementsByClassName("control-list-ele"),
+    tip = document.getElementById("tip"),
+    shipControl = document.getElementById("control-list"),
+    createButton = document.getElementById("create");
+
+var Adapter = {
+  binaryToCommand: function binaryToCommand(data) {
+    var commondHash = {
+      "0001": "start",
+      "0010": "stop",
+      "1100": "delete"
+    };
+
+    var id = data.slice(0, 4).split('').reduce(function (prev, cur, index) {
+      if (parseInt(cur)) {
+        return parseInt(prev) + Math.pow(2, 3 - index);
+      } else {
+        return parseInt(prev);
+      }
+    });
+    var commond = data.slice(4);
+
+    return {
+      id: id,
+      commond: commondHash[commond]
+    };
+  },
+  commondTobinary: function commondTobinary(id, action) {
+    var commondHash = {
+      "start": "0001",
+      "stop": "0010",
+      "delete": "1100"
+    };
+
+    var res = new Array('0', '0', '0', '0');
+    var count = 4;
+
+    while (count--) {
+      if (id % 2) res[count] = 1;
+      id = Math.floor(id / 2);
+      if (!id) break;
+    }
+
+    return res.join('') + commondHash[action];
+  }
+};
+
+var DC = {};
 
 var Ship = function () {
   function Ship(id) {
     _classCallCheck(this, Ship);
 
+    // 飞船编号
     this.id = id;
+    // 移动事件句柄
     this.event = null;
-    this.deg = 0;
     this.isStart = 0;
+    this.isCreate = 0;
+    // 当前飞行角度
+    this.deg = 0;
+    // 当前能量值
+    this.power = 100;
+    // 飞行角度速率
+    this.degSpeed = 0;
+    // 能量消耗速率
+    this.powerSpeed = 0;
+    // 补给速率
+    this.supplySpeed = 0;
+    // 广播句柄
+    this.sendState = null;
   }
 
   _createClass(Ship, [{
-    key: 'start',
-    value: function start() {
-      this.isStart = 1;
-      var self = this;
-      this.event = setInterval(function () {
-        self.transform();
-        self.deg++;
-        if (self.deg === 360) self.deg = 0;
-      }, 50);
-
-      //let element = document.creatElement('li');
-      //element.setAttribute('id', 'control' + this.id);
+    key: "create",
+    value: function create(degSpeed, powerSpeed, supplySpeed) {
+      this.isCreate = 1;
+      this.degSpeed = 3 * degSpeed / (Math.PI * orbitRadius);
+      this.supplySpeed = supplySpeed;
+      this.powerSpeed = powerSpeed;
+      shipDOM[this.id].style.display = 'block';
+      shipDOM[this.id].textContent = this.id + '号 - ' + this.power + '%';
+      shipControlList[this.id].style.display = 'block';
+      this.sendDC();
     }
   }, {
-    key: 'stop',
+    key: "start",
+    value: function start() {
+      // 启动飞船，更改飞船状态
+      if (this.isStart) {
+        return;
+      } else {
+        this.isStart = 1;
+      }
+      var self = this;
+      var count = 0;
+
+      var step = function step() {
+        self.deg = self.deg + self.degSpeed;
+        // requestAnimationFrame是每秒60帧，每当count为6时，触发能量计算操作
+        if (count === 6) {
+          self.power = self.power - (self.powerSpeed - self.supplySpeed) / 10;
+          if (self.power < 0) self.power = 0;
+          if (self.power > 100) self.power = 100;
+          shipDOM[self.id].textContent = self.id + '号 - ' + Math.ceil(self.power) + '%';
+          if (self.power === 0) {
+            self.stop();return;
+          }
+          count = 0;
+        } else {
+          count++;
+        }
+        self.transform();
+        if (self.deg >= 359.9) self.deg = 0;
+        self.event = window.requestAnimationFrame(step);
+      };
+      this.event = window.requestAnimationFrame(step);
+    }
+  }, {
+    key: "stop",
     value: function stop() {
       this.isStart = 0;
-      clearInterval(this.event);
+      window.cancelAnimationFrame(this.event);
       this.event = null;
     }
   }, {
-    key: 'transform',
+    key: "delete",
+    value: function _delete() {
+      this.isCreate = 0;
+      var target = shipDOM[this.id];
+      target.style.display = "none";
+      target.style.top = "";
+      target.style.left = "";
+      target.style.webkitTransform = "rotate(0deg)";
+      this.deg = 0;
+      this.power = 100;
+      shipDOM[this.id].textContent = this.id + '号 - ' + this.power + '%';
+      shipControlList[this.id].style.display = "none";
+      tip.textContent = "";
+
+      clearInterval(this.sendState);
+      this.sendState = null;
+    }
+  }, {
+    key: "transform",
     value: function transform() {
       var target = shipDOM[this.id],
           deg = this.deg,
@@ -77,13 +190,44 @@ var Ship = function () {
       target.style.transform = "rotate(-" + deg + "deg)";
       target.style.webkitTransform = "rotate(-" + deg + "deg)";
     }
+  }, {
+    key: "receive",
+    value: function receive(data) {
+      var sendPackage = Adapter.binaryToCommand(data);
+
+      var id = parseInt(sendPackage.id);
+
+      if (id === this.id) {
+        switch (sendPackage.commond) {
+          case "start":
+            this.start();
+            tip.textContent = this.id + "号飞船已启动";
+            break;
+          case "stop":
+            this.stop();
+            tip.textContent = this.id + "号飞船已停止";
+            break;
+          case "delete":
+            this.delete();
+            tip.textContent = this.id + "号飞船已销毁";
+            break;
+          default:
+            tip.textContent = "无效命令";
+        }
+      }
+    }
+  }, {
+    key: "sendDC",
+    value: function sendDC() {
+      this.sendState = setInterval(function () {}, 1000);
+    }
   }]);
 
   return Ship;
 }();
 
-Ship.width = 60;
-Ship.height = 35;
+Ship.width = 90;
+Ship.height = 40;
 
 var ShipList = function () {
   function ShipList() {
@@ -97,16 +241,25 @@ var ShipList = function () {
   }
 
   _createClass(ShipList, [{
-    key: 'findNoStartShip',
-    value: function findNoStartShip() {
+    key: "findNoCreateShip",
+    value: function findNoCreateShip() {
       var resId = -1;
       for (var i = 0; i < this.nowQueue.length; i++) {
-        if (resId === -1 && this.nowQueue[i].isStart === 0) {
+        if (resId === -1 && this.nowQueue[i].isCreate === 0) {
           resId = this.nowQueue[i].id;
           break;
         }
       }
       return resId;
+    }
+  }, {
+    key: "broadcast",
+    value: function broadcast(sendPackage) {
+      this.nowQueue.forEach(function (item, index) {
+        if (item.isCreate) {
+          item.receive(sendPackage);
+        }
+      });
     }
   }]);
 
@@ -115,14 +268,34 @@ var ShipList = function () {
 
 var shipList = new ShipList();
 
-var createButton = document.getElementById("create");
 createButton.addEventListener("click", function (e) {
-  var id = shipList.findNoStartShip();
+  var id = shipList.findNoCreateShip();
   if (id === -1) {
-    var tip = document.getElementById("tip");
-    tip.textContent = "飞船已全部启动";
+    tip.textContent = "飞船已全部被创建";
   } else {
-    var ship = shipList.nowQueue[id];
-    ship.start();
+    var select = document.querySelectorAll("input:checked");
+    var degSpeed = select[0].getAttribute("data-speed"),
+        powerSpeed = select[0].getAttribute("data-power"),
+        supplySpeed = select[1].getAttribute("data-supply");
+
+    shipList.nowQueue[id].create(degSpeed, powerSpeed, supplySpeed);
+  }
+}, false);
+
+shipControl.addEventListener("click", function (e) {
+  var id = e.target.getAttribute("data-id");
+  var action = e.target.getAttribute("data-action");
+
+  var sendPackage = Adapter.commondTobinary(id, action);
+
+  var probability = parseInt(Math.random() * 100);
+  if (probability > PACKET_LOSS) {
+    setTimeout(function () {
+      shipList.broadcast(sendPackage);
+    }, TIME_DEFER);
+    tip.textContent = "命令已下达";
+  } else {
+    var _tip = document.getElementById("tip");
+    _tip.textContent = "命令未到达";
   }
 }, false);
--- task-section-2/task26-28/main.js
+++ task-section-2/task26-28/main.js
@@ -3,37 +3,148 @@ const orbitRadius = 125,
       mainWidth = 380,
       shipCount = 4;
 
-let shipDOM = document.getElementsByClassName('ship'),
-    controlList = document.getElementById('control-list');
+const TIME_DEFER = 300,
+      PACKET_LOSS = 10;
+
+let shipDOM = document.getElementsByClassName("ship"),
+    shipControlList = document.getElementsByClassName("control-list-ele"),
+    tip = document.getElementById("tip"),
+    shipControl = document.getElementById("control-list"),
+    createButton = document.getElementById("create");
+
+const Adapter = {
+  binaryToCommand: function(data) {
+    const commondHash = {
+      "0001": "start",
+      "0010": "stop",
+      "1100": "delete"
+    };
+
+    let id = data.slice(0, 4).split('').reduce(function(prev, cur, index){
+      if(parseInt(cur)) {
+        return parseInt(prev) + Math.pow(2, 3 - index);
+      } else {
+        return parseInt(prev);
+      }
+    });
+    let commond = data.slice(4);
+
+    return {
+      id: id,
+      commond: commondHash[commond]
+    }
+  },
+  commondTobinary: function(id, action) {
+    const commondHash = {
+      "start": "0001",
+      "stop": "0010",
+      "delete": "1100"
+    };
+
+    let res = new Array('0', '0', '0', '0');
+    let count = 4;
+
+    while(count--) {
+      if(id % 2) res[count] = 1;
+      id = Math.floor(id / 2);
+      if(!id) break;
+    }
+
+    return res.join('') + commondHash[action];
+  }
+};
+
+const DC = {
+
+};
 
 class Ship {
-  static width = 60;
-  static height = 35;
+  static width = 90;
+  static height = 40;
 
   constructor(id) {
+    // 飞船编号
     this.id = id;
+    // 移动事件句柄
     this.event = null;
-    this.deg = 0;
     this.isStart = 0;
+    this.isCreate = 0;
+    // 当前飞行角度
+    this.deg = 0;
+    // 当前能量值
+    this.power = 100;
+    // 飞行角度速率
+    this.degSpeed = 0;
+    // 能量消耗速率
+    this.powerSpeed = 0;
+    // 补给速率
+    this.supplySpeed = 0;
+    // 广播句柄
+    this.sendState = null;
+  }
+
+  create(degSpeed, powerSpeed, supplySpeed) {
+    this.isCreate = 1;
+    this.degSpeed = (3 * degSpeed) / (Math.PI * orbitRadius);
+    this.supplySpeed = supplySpeed;
+    this.powerSpeed = powerSpeed
+    shipDOM[this.id].style.display = 'block';
+    shipDOM[this.id].textContent = this.id + '号 - ' + this.power + '%';
+    shipControlList[this.id].style.display = 'block';
+    this.sendDC();
   }
 
   start() {
-    this.isStart = 1;
+    // 启动飞船，更改飞船状态
+    if(this.isStart){
+      return;
+    } else {
+      this.isStart = 1;
+    }
     let self = this;
-    this.event = setInterval(function(){
-      self.transform();
-      self.deg++;
-      if(self.deg === 360) self.deg = 0;
-    }, 50);
+    let count = 0;
 
-    //let element = document.creatElement('li');
-    //element.setAttribute('id', 'control' + this.id);
+    let step = function(){
+      self.deg = self.deg + self.degSpeed;
+      // requestAnimationFrame是每秒60帧，每当count为6时，触发能量计算操作
+      if(count === 6) {
+        self.power = self.power - (self.powerSpeed - self.supplySpeed) / 10;
+        if(self.power < 0) self.power = 0;
+        if(self.power > 100) self.power = 100;
+        shipDOM[self.id].textContent = self.id + '号 - ' + Math.ceil(self.power) + '%';
+        if(self.power === 0) { self.stop(); return; }
+        count = 0;
+      } else {
+        count++;
+      }
+      self.transform();
+      if(self.deg >= 359.9) self.deg = 0;
+      self.event = window.requestAnimationFrame(step);
+    }
+    this.event = window.requestAnimationFrame(step);
   }
 
   stop() {
     this.isStart = 0;
-    clearInterval(this.event);
+    window.cancelAnimationFrame(this.event);
     this.event = null;
+  }
+
+  delete() {
+    this.isCreate = 0;
+    let target = shipDOM[this.id];
+    target.style.display = "none";
+    target.style.top = "";
+    target.style.left = "";
+    target.style.webkitTransform = "rotate(0deg)";
+    this.deg = 0;
+    this.power = 100;
+    shipDOM[this.id].textContent = this.id + '号 - ' + this.power + '%';
+    shipControlList[this.id].style.display = "none";
+    tip.textContent = "";
+
+    clearInterval(this.sendState);
+    this.sendState = null;
   }
 
   transform() {
@@ -67,6 +178,34 @@ class Ship {
     target.style.webkitTransform = "rotate(-" + deg + "deg)";
   }
 
+  receive(data) {
+    const sendPackage = Adapter.binaryToCommand(data);
+
+    let id = parseInt(sendPackage.id);
+
+    if(id === this.id) {
+      switch (sendPackage.commond) {
+        case "start":
+          this.start();
+          tip.textContent = this.id + "号飞船已启动";
+          break;
+        case "stop":
+          this.stop();
+          tip.textContent = this.id + "号飞船已停止";
+          break;
+        case "delete":
+          this.delete();
+          tip.textContent = this.id + "号飞船已销毁";
+          break;
+        default:
+          tip.textContent = "无效命令";
+      }
+    }
+  }
+
+  sendDC() {
+    this.sendState = setInterval(function(){}, 1000);
+  }
 }
 
 class ShipList {
@@ -78,10 +217,10 @@ class ShipList {
     }
   }
 
-  findNoStartShip() {
+  findNoCreateShip() {
     let resId = -1;
     for(let i = 0; i < this.nowQueue.length; i++) {
-      if(resId === -1 && this.nowQueue[i].isStart === 0) {
+      if(resId === -1 && this.nowQueue[i].isCreate === 0) {
         resId = this.nowQueue[i].id;
         break;
       }
@@ -89,18 +228,46 @@ class ShipList {
     return resId;
   }
 
+  broadcast(sendPackage) {
+    this.nowQueue.forEach(function(item, index) {
+      if(item.isCreate) {
+        item.receive(sendPackage);
+      }
+    });
+  }
 }
 
 let shipList = new ShipList();
 
-const createButton = document.getElementById("create");
 createButton.addEventListener("click", function(e) {
-  let id = shipList.findNoStartShip();
+  let id = shipList.findNoCreateShip();
   if(id === -1) {
-    let tip = document.getElementById("tip");
-    tip.textContent = "飞船已全部启动";
+    tip.textContent = "飞船已全部被创建";
   } else {
-    let ship = shipList.nowQueue[id];
-    ship.start();
+    let select = document.querySelectorAll("input:checked");
+    let degSpeed = select[0].getAttribute("data-speed"),
+        powerSpeed = select[0].getAttribute("data-power"),
+        supplySpeed = select[1].getAttribute("data-supply");
+
+    shipList.nowQueue[id].create(degSpeed, powerSpeed, supplySpeed);
   }
+}, false);
+
+shipControl.addEventListener("click", function(e) {
+  let id = e.target.getAttribute("data-id");
+  let action = e.target.getAttribute("data-action");
+
+  const sendPackage = Adapter.commondTobinary(id, action);
+
+  let probability = parseInt(Math.random() * 100);
+  if(probability > PACKET_LOSS) {
+    setTimeout(function(){
+      shipList.broadcast(sendPackage);
+    }, TIME_DEFER);
+    tip.textContent = "命令已下达";
+  } else {
+    let tip = document.getElementById("tip");
+    tip.textContent = "命令未到达";
+  }
+
 }, false);
